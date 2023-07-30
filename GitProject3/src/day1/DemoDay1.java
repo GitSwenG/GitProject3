fcc99105496dd827f4b812bcfe3b8fb3ab305dcf
vitai <vitai@HappyTrees.hsd1.fl.comcast.net>
2023-07-30 06:51:09 -0700

    Created project class

---
+++ GitProject3/src/day1/DemoDay1.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class DemoDay1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		
+	}
+	
+	public static void info() {
+		
+		System.out.println(" today");
+	}
+}
